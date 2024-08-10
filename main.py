5065859d502a06e5c58c17a31eac407b5da266f1
k0rsakov <korsakov.iyu@gmail.com>
2024-08-10 05:39:55 +0300

    Change project infrastructure_for_data_engineer_kafka: - work main.py

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
+import json
+import time
+import random
 from confluent_kafka import Producer
 
-conf = {
-    'bootstrap.servers': 'localhost:9092'
-}
+# Define the Kafka configuration
+conf = {'bootstrap.servers': "localhost:19092"}
 
-producer = Producer(**conf)
+# Create a Producer instance with the above configuration
+producer = Producer(conf)
 
-def delivery_report(err, msg):
-    if err is not None:
-        print(f'Message delivery failed: {err}')
-    else:
-        print(f'Message delivered to {msg.topic()} [{msg.partition()}]')
+while True:
+    # Define some data to send to Kafka
+    data = {
+        "user_id": random.randint(1, 100),
+        "first_name": "John",
+        "message": f"Random Message {random.randint(1, 1000)}"
+    }
 
-for i in range(10):
-    producer.produce('my_topic', key=str(i), value=f'message-{i}', callback=delivery_report)
-    producer.poll(0)
+    # Convert the data to a JSON string
+    data_str = json.dumps(data)
 
-producer.flush()
+    # Produce a message to the "test" topic
+    producer.produce(topic="my_topic", value=data_str)
+
+    # Flush the producer to ensure all messages are sent
+    producer.flush()
+
+    # Sleep for a second before producing the next set of messages
+    time.sleep(3)
