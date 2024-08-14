62db569df0b1215eef2aa6ca449dc1306e22d546
k0rsakov <korsakov.iyu@gmail.com>
2024-08-14 06:32:39 +0300

    Change project infrastructure_for_data_engineer_kafka: - Изменен список зависимостей - Изменен пример для producer

--- simple_producer.py
+++ simple_producer.py
@@ -1,7 +1,22 @@
 import json
 import time
-import random
 from confluent_kafka import Producer
+from faker import Faker
+import uuid_utils as uuid
+
+
+def generate_list_of_dict() -> dict[str, str]:
+    fake = Faker(locale='ru_RU')
+
+    dict_ = {
+        'uuid': str(uuid.uuid7()),
+        'first_name': fake.first_name(),
+        'last_name': fake.last_name(),
+        'middle_name': fake.middle_name(),
+    }
+
+    return dict_
+
 
 # Define the Kafka configuration
 conf = {'bootstrap.servers': "localhost:19092"}
@@ -9,18 +24,15 @@ conf = {'bootstrap.servers': "localhost:19092"}
 # Create a Producer instance with the above configuration
 producer = Producer(conf)
 
+
 while True:
     # Define some data to send to Kafka
-    data = {
-        "user_id": random.randint(1, 100),
-        "first_name": "John",
-        "message": f"Random Message {random.randint(1, 1000)}"
-    }
+    data = generate_list_of_dict()
 
     # Convert the data to a JSON string
     data_str = json.dumps(data)
 
-    # Produce a message to the "test" topic
+    # Produce a message to the "my_topic" topic
     producer.produce(topic="my_topic", value=data_str)
 
     # Flush the producer to ensure all messages are sent
